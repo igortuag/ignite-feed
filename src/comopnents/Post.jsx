fbd3c9df44bed98a5e6d23538078b414641c8369
Tuag <igortuag@gmail.com>
2025-10-08 09:00:21 -0300

    feat: style Post component with CSS module for improved layout and design

--- src/comopnents/Post.jsx
+++ src/comopnents/Post.jsx
@@ -1,9 +1,11 @@
+import Styles from "./Post.module.css";
+
 export default function Post({ author, content, publishedAt }) {
   return (
-    <article>
-      <h2>{author}</h2>
-      <p>{content}</p>
-      <time>{publishedAt}</time>
+    <article className={Styles.post}>
+      <h2 className={Styles.author}>{author}</h2>
+      <p className={Styles.content}>{content}</p>
+      <time className={Styles.publishedAt}>{publishedAt}</time>
     </article>
   );
 }
